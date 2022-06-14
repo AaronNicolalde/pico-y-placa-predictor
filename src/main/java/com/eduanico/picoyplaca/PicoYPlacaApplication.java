42a60b1c626d37e1f0e3e1074523a3579f3c730b
AaronNicolalde <eduanico@espol.edu.ec>
2022-06-14 17:07:55 -0500

    Creation of project

---
+++ src/main/java/com/eduanico/picoyplaca/PicoYPlacaApplication.java
@@ -0,0 +1,13 @@
+package com.eduanico.picoyplaca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicoYPlacaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicoYPlacaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduanico/picoyplaca/PicoYPlacaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduanico.picoyplaca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicoYPlacaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
