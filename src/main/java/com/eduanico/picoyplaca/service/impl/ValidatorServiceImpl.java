a006c394239fa479039a56266cbb6c8082a89f0b
AaronNicolalde <eduanico@espol.edu.ec>
2022-06-14 20:39:49 -0500

    Validation on plate numer format.

--- src/main/java/com/eduanico/picoyplaca/service/impl/ValidatorServiceImpl.java
+++ src/main/java/com/eduanico/picoyplaca/service/impl/ValidatorServiceImpl.java
@@ -12,12 +12,21 @@ import java.util.regex.Pattern;
 @Service
 public class ValidatorServiceImpl implements ValidatorService {
 
-
+    /*
+    Return true if plateNumber matches pattern @@@-####, if not return false.
+     */
     @Override
     public boolean validatePlateNumberFormat(String plateNumber) {
-        return true;
+        Pattern pattern = Pattern.compile("([A-Z][A-Z][A-Z])-[0-9][0-9][0-9][0-9]?");
+
+        Matcher matcher = pattern.matcher(plateNumber);
+
+        return matcher.matches();
     }
 
+    /*
+    Return true if date matches pattern dd/MM/yyyy, if not return exception and false.
+     */
     @Override
     public boolean validateDateFormat(String date) {
         DateTimeFormatter PARSE_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
@@ -30,6 +39,9 @@ public class ValidatorServiceImpl implements ValidatorService {
         }
     }
 
+    /*
+    Return true if time matches pattern hh:mm, if not return false.
+     */
     @Override
     public boolean validateTimeFormat(String time) {
         Pattern pattern = Pattern.compile("([0-1][0-9]|2[0-3]):[0-5][0-9]");
